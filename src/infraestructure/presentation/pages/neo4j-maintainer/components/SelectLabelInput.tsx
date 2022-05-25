8a413ef901879236d97ac388e1a33c5b1d0daae4
Red Benjamin <red@benjamin.com>
2022-05-25 00:10:14 -0400

    SelectNodeInput refactorized

--- src/infraestructure/presentation/pages/neo4j-maintainer/components/SelectLabelInput.tsx
+++ src/infraestructure/presentation/pages/neo4j-maintainer/components/SelectLabelInput.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import FormControl from '@mui/material/FormControl';
 import Chip from '@mui/material/Chip';
 import { getLabels } from '../../../../../core/domain/service/getLabels';
--- src/infraestructure/presentation/pages/neo4j-maintainer/components/SelectNodeInput.tsx
+++ src/infraestructure/presentation/pages/neo4j-maintainer/components/SelectNodeInput.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import { getNodeTypes } from '../../../../../core/domain/service/getNodeTypes';
@@ -15,50 +15,57 @@ export default function SelectNodeInput(props: CustomInputProps<string>) {
 
   const { value, onChange } = props;
 
+  const handleOnChange = (event: any, newValue: any) => {
+    if (typeof newValue === 'string') {
+      onChange(newValue);
+    } else if (newValue && newValue.inputValue) {
+      // Create a new value from the user input
+      onChange(newValue.inputValue);
+    } else if (newValue) {
+      onChange(newValue.title);
+    }
+  }
+
+  const filterOptions = (options: NodeOptionType[], params: any) => {
+    const filtered = filter(options, params);
+    const { inputValue } = params;
+    // Suggest the creation of a new value
+    const isExisting = options.some((option) => inputValue === option.title);
+    if (inputValue !== '' && !isExisting) {
+      filtered.push({
+        inputValue,
+        title: `Crear Nodo "${inputValue}"`,
+      });
+    }
+    return filtered;
+  }
+
+  const getOptionLabel = (option: any) => {
+    // Value selected with enter, right from the input
+    if (typeof option === 'string') {
+      return option;
+    }
+    // Add "xxx" option created dynamically
+    if (option.inputValue) {
+      return option.inputValue;
+    }
+    // Regular option
+    return option.title;
+  }
+
+  const nodeOptions = getNodeTypes().map(x => ({ title: x }as NodeOptionType))
 
   return (
     <Autocomplete
       value={value}
-      onChange={(event, newValue) => {
-        if (typeof newValue === 'string') {
-          onChange(newValue);
-        } else if (newValue && newValue.inputValue) {
-          // Create a new value from the user input
-          onChange(newValue.inputValue);
-        } else if (newValue) {
-          onChange(newValue.title);
-        }
-      }}
-      filterOptions={(options, params) => {
-        const filtered = filter(options, params);
-        const { inputValue } = params;
-        // Suggest the creation of a new value
-        const isExisting = options.some((option) => inputValue === option.title);
-        if (inputValue !== '' && !isExisting) {
-          filtered.push({
-            inputValue,
-            title: `Crear Nodo "${inputValue}"`,
-          });
-        }
-        return filtered;
-      }}
+      onChange={handleOnChange}
+      filterOptions={filterOptions}
       selectOnFocus
       clearOnBlur
       handleHomeEndKeys
       id="node-type"
-      options={getNodeTypes().map(x => ({ title: x }as NodeOptionType))}
-      getOptionLabel={(option) => {
-        // Value selected with enter, right from the input
-        if (typeof option === 'string') {
-          return option;
-        }
-        // Add "xxx" option created dynamically
-        if (option.inputValue) {
-          return option.inputValue;
-        }
-        // Regular option
-        return option.title;
-      }}
+      options={nodeOptions}
+      getOptionLabel={getOptionLabel}
       renderOption={(props, option) => <li {...props}>{option.title}</li>}
       freeSolo
       renderInput={(params) => (
@@ -68,27 +75,3 @@ export default function SelectNodeInput(props: CustomInputProps<string>) {
   );
 }
 
-// export default function SelectNodeInput() {
-//   const [age, setAge] = React.useState('');
-
-//   const handleChange = (event: SelectChangeEvent) => {
-//     setAge(event.target.value as string);
-//   };
-
-//   const nodeTypes = getNodeTypes()
-
-//   return (
-//       <FormControl fullWidth>
-//         <InputLabel id="node-select-label">Tipo Nodo</InputLabel>
-//         <Select
-//           labelId="node-select-label"
-//           id="node-select"
-//           value={age}
-//           label="Entidad"
-//           onChange={handleChange}
-//         >
-//           { nodeTypes.map(n => <MenuItem key={n} value={n}>{n}</MenuItem>)}
-//         </Select>
-//       </FormControl>
-//   );
-// }
