6a162ce2470fcbed1fa9caacfbe1f0fe6cfdd59e
Red Benjamin <red@benjamin.com>
2022-06-13 20:07:07 -0400

    fix Relationship name invalid by spaces and lowercase

--- src/infraestructure/presentation/pages/neo4j-maintainer/components/NewRelationshipInput.tsx
+++ src/infraestructure/presentation/pages/neo4j-maintainer/components/NewRelationshipInput.tsx
@@ -3,22 +3,22 @@ import TextField from '@mui/material/TextField';
 import { FormControl } from '@mui/material';
 import { CustomInputProps } from '../../../shared/utils/CustomInputProps';
 
-interface Props extends CustomInputProps<string> {
+interface Props extends CustomInputProps<string> { }
 
-}
+const transformValue = (value: string) => value? value.replaceAll(' ', '_').toUpperCase() : ''
+
 export default function NewRelationshipInput({ value, onChange }: Props) {
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => onChange(event.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => onChange(transformValue(event.target.value));
 
   return (
     <FormControl fullWidth>
       <TextField
         id="outlined-name"
         label="Nueva relación"
-        value={value? value.replaceAll(' ', '_') : ''}
+        value={transformValue(value)}
         placeholder="Ingrese el nombre de la relacion"
         onChange={handleChange}
-        inputProps={{ style: { textTransform: "uppercase" } }}
       />
     </FormControl>
   );
